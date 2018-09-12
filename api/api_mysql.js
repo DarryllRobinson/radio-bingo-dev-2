045360ef7c90bc357a3a1317cf8070e172fc9186
DarryllRobinson <darryll@stillproud.com>
2018-09-12 21:15:29 +0200

    MySQL finished?

--- api/api_mysql.js
+++ api/api_mysql.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const api_mysqlRouter = express.Router();
-const artists_mysqlRouter = require('./artists_mysql.js');
+const apiRouter = express.Router();
+const songsRouter = require('./songs.js');
+const artistsRouter = require('./artists.js');
+const usersRouter = require('./users.js');
+const cardsRouter = require('./cards.js');
+const tilesRouter = require('./tiles.js');
+const rdsRouter = require('./rds.js');
+const campaignsRouter = require('./campaigns.js');
 
-apiRouter.use('/artists_mysql', artists_mysqlRouter);
+apiRouter.use('/songs', songsRouter);
+apiRouter.use('/artists', artistsRouter);
+apiRouter.use('/users', usersRouter);
+apiRouter.use('/cards', cardsRouter);
+apiRouter.use('/tiles', tilesRouter);
+apiRouter.use('/rds', rdsRouter);
+apiRouter.use('/campaigns', campaignsRouter);
 
 module.exports = api_mysqlRouter;
--- server_mysql.js
+++ server_mysql.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection( {
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'mydb'
+  database: 'radio_bingo'
 });
 
 connection.connect(function(err) {
